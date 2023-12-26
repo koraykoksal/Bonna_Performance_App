0009f52be874d7d1632e766ca77e0f1cd7a0aa1e
koraykoksal <kksl.koray@gmail.com>
2023-12-26 08:33:34 +0300

    vite config

--- v2_by/vite.config.js
+++ v2_by/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -19,12 +20,7 @@ export default defineConfig({
         target: 'https://api.twiser.com',
         changeOrigin: true,
         secure: false,
-      },
-      '/butunbiApi': {
-        target: 'http://176.236.227.82:3019',
-        changeOrigin: true,
-        secure: false,
-      },
+      }
     }
   }
 })
